8cf665bc322bb64f3a44ffd0a03d21f18e0afda4
Noah Verner <83596569+noahverner1995@users.noreply.github.com>
2022-05-18 19:48:29 -0500

    Improving the code
    Now it's not necessary anymore the NumPy library nor other complicated stuff

--- Data gathering/data_downloader/automated_data_downloader_for_binance_futures.py
+++ Data gathering/data_downloader/automated_data_downloader_for_binance_futures.py
@@ -6,11 +6,10 @@ Created on Sun Feb 13 17:40:21 2022
 """
 
 import time
-import numpy as np
+from datetime import date, timedelta
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
 
 opt = Options() #the variable that will store the selenium options
 opt.add_experimental_option("debuggerAddress", "localhost:9222") #this allows bulk-dozer to take control of your Chrome Browser in DevTools mode.
@@ -19,22 +18,30 @@ driver = webdriver.Chrome(service=s, options=opt) #execute the chromedriver.exe 
 
 #Why using MarkPrices: https://support.btse.com/en/support/solutions/articles/43000557589-index-price-and-mark-price#:~:text=Index%20Price%20is%20an%20important,of%20cryptocurrencies%20on%20major%20exchanges.&text=Mark%20Price%20is%20the%20price,be%20fair%20and%20manipulation%20resistant.
 
-if driver.current_url == 'https://data.binance.vision/?prefix=data/futures/um/daily/markPriceKlines/ALICEUSDT/1h/' :  #ALICEUSDT is shown as a sample.
-    number = 2 #initialize an int variable to 2 because the desired web elements in this page starts from 2
-    counter = 0
-    the_dictionary_links = {}
-    while number <= np.size(driver.find_elements(By.XPATH, '//*[@id="listing"]/tr')): #iterate over the tbody array
-        data_file_name = driver.find_element(By.XPATH, f'//*[@id="listing"]/tr[{number}]/td[1]/a').text
-        if data_file_name.endswith('CHECKSUM') == False:
-            the_dictionary_links[data_file_name] = driver.find_element(By.XPATH, f'//*[@id="listing"]/tr[{number}]/td[1]/a').get_attribute('href')
-            print(f'Saving {data_file_name} and its link for later use')
-            counter += 1            
-        number += 1
-    print(counter)
-    i = 0
-    o = 0
-    for i,o in the_dictionary_links.items():
-        driver.get(o)
-        print(f'Downloading {i}')
-        time.sleep(1.8)
+if driver.current_url == 'https://data.binance.vision/?prefix=data/futures/um/daily/markPriceKlines/XRPUSDT/1h/':
+    
+    #Set your desired start date and end date
+    start_date = date(2022, 1, 2)
+    end_date = date(2022, 2, 13)
+    delta = timedelta(days=1)
+    
+    #iterate over the dates
+    while start_date <= end_date:
+        
+        date_string = str(start_date)
+        
+        filename = "XRPUSDT"+"-"+"1h"+"-"+date_string+".zip"
+        
+        #create a download link for this particular page
+        download_link = driver.current_url+filename
+        
+        #go to the download link
+        driver.get(download_link)
+        
+        #print what's being downloaded and wait 2 seconds before repeating the process with the next date
+        print(f'Downloading {filename}')
+        time.sleep(2)
+        
+        #count it
+        start_date += delta
         
